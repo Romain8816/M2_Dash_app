b7b69dc50bf57636de4b0b39ef74f09d4d90f92b
Romain Dudoit <ro.dudoit@gmail.com>
2021-10-15 22:56:18 +0200

    sélection de la variable cible

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input,Output
@@ -13,31 +14,39 @@ app.title="Machine Learning App"
 data_path = os.getcwd() +'\\data\\'
 files = [f for f in os.listdir(data_path)]
 
-app.layout = (html.Div(children=[
+app.layout = html.Div(children=[
         html.H1('Machine Learning App',className='h1'),
 
         html.P('Application réalisée par Olivier IMBAUD, Inès KARA, Romain DUDOIT'),
-        html.P('Sélectionner un jeu de données'),
         dcc.Dropdown(
             id='file_selection',
             options=[
                 {'label':i, 'value':i} for i in files
-            ]), 
-        dcc.Dropdown(
-            id='variable_cible'
-        )
+            ],
+            searchable=False,
+            placeholder="Choisir un jeu de données",
+            clearable=False
+        ), 
+
+        html.Div([
+            dcc.Dropdown(id='variable_cible', placeholder="Sélectionner la variable cible", searchable=False, style={'display':'none'}),
+        ])
+        
     ])
-)
 
 @app.callback(
+    Output(component_id='variable_cible', component_property='style'),
     Output(component_id='variable_cible', component_property='options'),
     Input(component_id='file_selection', component_property='value')
 )
-def selection_variable_cible(file):
-    df = pd.read_csv(data_path+'iris_data.csv')
-    variable_list= list(df.columns.values)
-    return ({'label':v, 'value':v} for v in variable_list)
-
+def selection_variable_cible(input_value):
+    # On commence d'abord par traiter le cas lorsque l'utilisateur n'a rien sélectionné
+    if input_value is None:
+        raise PreventUpdate
+    else :
+        df = pd.read_csv(data_path+input_value)
+        variable_list= list(df.columns.values)
+        return {'display': 'block'}, [{'label':v, 'value':v} for v in variable_list]
 
 
 if __name__=='__main__':
