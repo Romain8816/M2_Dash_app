547dc6a9d5ce69087a49fa15cbf75c0d03f55e53
Romain8816 <ro.dudoit@gmail.com>
2021-10-15 20:14:53 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input,Output
 import os
+import pandas as pd
 
 app = dash.Dash(__name__)
 app.title="Machine Learning App"
@@ -16,14 +17,28 @@ app.layout = (html.Div(children=[
         html.H1('Machine Learning App',className='h1'),
 
         html.P('Application réalisée par Olivier IMBAUD, Inès KARA, Romain DUDOIT'),
-    
+        html.P('Sélectionner un jeu de données'),
         dcc.Dropdown(
-            id='test',
+            id='file_selection',
             options=[
                 {'label':i, 'value':i} for i in files
-            ])
+            ]), 
+        dcc.Dropdown(
+            id='variable_cible'
+        )
     ])
 )
+
+@app.callback(
+    Output(component_id='variable_cible', component_property='options'),
+    Input(component_id='file_selection', component_property='value')
+)
+def selection_variable_cible(file):
+    df = pd.read_csv(data_path+'iris_data.csv')
+    variable_list= list(df.columns.values)
+    return ({'label':v, 'value':v} for v in variable_list)
+
+
 
 if __name__=='__main__':
     app.run_server(debug=True)
